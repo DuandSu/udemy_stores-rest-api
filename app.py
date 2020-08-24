c6248f61a4099886addbfdcd9c4404a000e30b30
DuandSu <duane.w.munro@gmail.com>
2020-08-23 22:14:56 -0600

    23-Aug-2020: RAwFaP: Fixed not importing db when running the app.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from db import db
+# from db import db
 
 from flask import Flask
 from flask_restful import Api
@@ -15,10 +15,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity) # creates new end point /auth
 
 api.add_resource(Item, '/item/<string:name>')
@@ -29,5 +25,6 @@ api.add_resource(UserRegister, '/register')
 
 # port 5000 is default so argument not really necessary.
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
